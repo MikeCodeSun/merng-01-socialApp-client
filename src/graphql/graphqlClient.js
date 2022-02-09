dbbd81893b99f661aa6fb0c7f077ae2048ac5065
mike <mike@mikedeiMac.local>
2022-02-09 18:40:50 +0800

    server base url

--- src/graphql/graphqlClient.js
+++ src/graphql/graphqlClient.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/",
+  uri: "https://social-app-0-1.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
